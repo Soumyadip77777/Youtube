3171c5a04deb80d49b8f5caf04b8ca7337707ca1
Soumyadip Chatterjee <113227471+Soumyadip77777@users.noreply.github.com>
2025-03-23 23:30:34 +0530

    new

--- src/videos.ts
+++ src/videos.ts
@@ -36,6 +36,48 @@ export const VIDEOS: Video[] = [
         timestamp: "1 year ago",
     },
     {
+        title: "The DAY I Met JEET & PROSENJIT Dada | Khakee: The Bengal Chapter | The Bong Guy",
+        thumbnail: "/photo5.jpg",
+        description: "Video by Netflix India",
+        viewCount: "401K views",
+        timestamp: "9 days ago",
+    },
+    {
+        title: "NEAZY ON EMIWAY VS KRSNA 😱 HONEY SINGH ROAST BADSHAH, RAFTAAR & MC...",
+        thumbnail: "/photo6.jpg",
+        description: "Video by Ashish Says",
+        viewCount: "313 views",
+        timestamp: "22 hours ago",
+    },
+    {
+        title: "Argentina v France: Full Penalty Shoot-out | 2022 #FIFAWorldCup Final",
+        thumbnail: "/photo7.jpg",
+        description: "Video by FIFA",
+        viewCount: "21M views",
+        timestamp: "1 year ago",
+    },
+    {
+        title: "Letting 833,059 people edit my homework",
+        thumbnail: "/photo8.jpg",
+        description: "Video by Beluga",
+        viewCount: "4.3M views",
+        timestamp: "1 month ago",
+    },
+    {
+        title: "New Samsung Pro Laptop with Amoled Display!",
+        thumbnail: "/photo9.jpg",
+        description: "Video by Gyan Therapy",
+        viewCount: "166K views",
+        timestamp: "48 minutes ago",
+    },
+    {
+        title: "TRY TO SURVIVE THESE IMPOSSIBLE CHALLENGES",
+        thumbnail: "/photo10.jpg",
+        description: "Video by S8UL",
+        viewCount: "126K views",
+        timestamp: "5 hours ago",
+    },
+    {
         title: "IPL Memes Are Too Funny😂 || Men Of Culture || Ep. 30",
         thumbnail: "/photo1.jpg",
         description: "video by Man of Culture",
@@ -62,7 +104,8 @@ export const VIDEOS: Video[] = [
         description: "video by Chai aur Code",
         viewCount: "340k views",
         timestamp: "1 year ago",
-    }
+    },
+    
     ]
     
 
