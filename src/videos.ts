281ca85686e815f19f0638843da89fc81bb20275
Soumyadip Chatterjee <113227471+Soumyadip77777@users.noreply.github.com>
2025-03-23 22:29:13 +0530

    new
    new

---
+++ src/videos.ts
@@ -0,0 +1,148 @@
+export interface Video {
+    title: string;
+    thumbnail: string;
+    description: string;
+    viewCount: string;
+    timestamp: string;
+}
+
+export const VIDEOS: Video[] = [
+    {
+        title: "IPL Memes Are Too Funny😂 || Men Of Culture || Ep. 30",
+        thumbnail: "/photo1.jpg",
+        description: "meme video",
+        viewCount: "1.4M views",
+        timestamp: "2 days ago",
+    },
+    {
+        title: "I Spent 100 Hours Inside The Pyramids!",
+        thumbnail: "/photo2.jpg",
+        description: "video by Mr.Beast",
+        viewCount: "1.4B views",
+        timestamp: "2 days ago",
+    },
+    {
+        title: "CIGARETTE - Farhan Khan (Prod.by Mr. Doss) ALIF LAILA THE ALBUM",
+        thumbnail: "/photo3.jpg",
+        description: "video by Faran Khan Music",
+        viewCount: "1M views",
+        timestamp: "1 day ago",
+    },
+    {
+        title: "DOM introduction in javascript | chai aur #javascript",
+        thumbnail: "/photo4.jpg",
+        description: "video by Chai aur Code",
+        viewCount: "340k views",
+        timestamp: "1 year ago",
+    },
+    {
+        title: "IPL Memes Are Too Funny😂 || Men Of Culture || Ep. 30",
+        thumbnail: "/photo1.jpg",
+        description: "video by Man of Culture",
+        viewCount: "1.4M views",
+        timestamp: "2 days ago",
+    },
+    {
+        title: "I Spent 100 Hours Inside The Pyramids!",
+        thumbnail: "/photo2.jpg",
+        description: "video by Mr.Beast",
+        viewCount: "1.4B views",
+        timestamp: "2 days ago",
+    },
+    {
+        title: "CIGARETTE - Farhan Khan (Prod.by Mr. Doss) ALIF LAILA THE ALBUM",
+        thumbnail: "/photo3.jpg",
+        description: "video by Faran Khan Music",
+        viewCount: "1M views",
+        timestamp: "1 day ago",
+    },
+    {
+        title: "DOM introduction in javascript | chai aur #javascript",
+        thumbnail: "/photo4.jpg",
+        description: "video by Chai aur Code",
+        viewCount: "340k views",
+        timestamp: "1 year ago",
+    }
+    ]
+    
+
+// export interface Video {
+//     title: string;
+//     thumbnail: string;
+//     description: string;
+//     viewCount: string;
+//     timestamp: string;
+// }
+
+// export const VIDEOS: Video[] = [
+//     {
+//         title: "New song",
+//         thumbnail: "/video1.jpg",
+//         description: "Mix Song of 2023 with dance and singing it in full stream",
+//         viewCount: "1.3k",
+//         timestamp: "4 hours ago"
+//     },
+//     {
+//         title: "New song",
+//         thumbnail: "/video1.jpg",
+//         description: "Mix Song of 2023 with dance and singing it in full stream",
+//         viewCount: "1.3k",
+//         timestamp: "4 hours ago"
+//     },
+//     {
+//         title: "New song",
+//         thumbnail: "/video1.jpg",
+//         description: "Mix Song of 2023 with dance and singing it in full stream",
+//         viewCount: "1.3k",
+//         timestamp: "4 hours ago"
+//     },
+//     {
+//         title: "New song",
+//         thumbnail: "/video1.jpg",
+//         description: "Mix Song of 2023 with dance and singing it in full stream",
+//         viewCount: "1.3k",
+//         timestamp: "4 hours ago"
+//     },
+//     {
+//         title: "New song",
+//         thumbnail: "/video1.jpg",
+//         description: "Mix Song of 2023 with dance and singing it in full stream",
+//         viewCount: "1.3k",
+//         timestamp: "4 hours ago"
+//     },
+//     {
+//         title: "New song",
+//         thumbnail: "/video1.jpg",
+//         description: "Mix Song of 2023 with dance and singing it in full stream",
+//         viewCount: "1.3k",
+//         timestamp: "4 hours ago"
+//     },
+//     {
+//         title: "New song",
+//         thumbnail: "/video1.jpg",
+//         description: "Mix Song of 2023 with dance and singing it in full stream",
+//         viewCount: "1.3k",
+//         timestamp: "4 hours ago"
+//     },
+//     {
+//         title: "New song",
+//         thumbnail: "/video1.jpg",
+//         description: "Mix Song of 2023 with dance and singing it in full stream",
+//         viewCount: "1.3k",
+//         timestamp: "4 hours ago"
+//     },
+//     {
+//         title: "New song",
+//         thumbnail: "/video1.jpg",
+//         description: "Mix Song of 2023 with dance and singing it in full stream",
+//         viewCount: "1.3k",
+//         timestamp: "4 hours ago"
+//     },
+//     {
+//         title: "New song",
+//         thumbnail: "/video1.jpg",
+//         description: "Mix Song of 2023 with dance and singing it in full stream",
+//         viewCount: "1.3k",
+//         timestamp: "4 hours ago"
+//     }
+// ]
